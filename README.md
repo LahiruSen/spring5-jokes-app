54ba2e3327032703da5411702845702fa6e4860c
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2021-04-28 23:02:27 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Boot Jokes App
+
+This spring boot web application displays random Chuck Norris Jokes
---
+++ src/main/java/tutorial/spring/jokesapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package tutorial.spring.jokesapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
